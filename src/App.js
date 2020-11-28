3553c184b4aef31e78dc1c147a00dc97efd6e4bf
JDeezy5291 <joshdemaria@aol.com>
2020-11-28 16:23:35 -0500

    functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,67 @@
  import { Component } from 'react';
 import './App.css';
 import MovieRow from './movieRow'
+import $ from 'jquery'
 
 class App extends Component {
   constructor(props) {
     super(props)
-    console.log("This is my initializer")
+    this.state = {}
+    // console.log("This is my initializer")
 
-    const movies = [
-      {id: 0, poster_src: "https://m.media-amazon.com/images/M/MV5BNjQ3NWNlNmQtMTE5ZS00MDdmLTlkZjUtZTBlM2UxMGFiMTU3XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_UX182_CR0,0,182,268_AL_.jpg", title: "Avengers: Infinity War", overview: "this is the overview"},
-      {id: 1, poster_src: "https://m.media-amazon.com/images/M/MV5BMTcxODgwMDkxNV5BMl5BanBnXkFtZTYwMDk2MDg3._V1_UX182_CR0,0,182,268_AL_.jpg", title: "Avengers: Infinity War 2", overview: "this is the overview 2"}
-    ]
+    // const movies = [
+    //   {id: 0, poster_src: "https://m.media-amazon.com/images/M/MV5BNjQ3NWNlNmQtMTE5ZS00MDdmLTlkZjUtZTBlM2UxMGFiMTU3XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_UX182_CR0,0,182,268_AL_.jpg", title: "Avengers: Infinity War", overview: "this is the overview"},
+    //   {id: 1, poster_src: "https://m.media-amazon.com/images/M/MV5BMTcxODgwMDkxNV5BMl5BanBnXkFtZTYwMDk2MDg3._V1_UX182_CR0,0,182,268_AL_.jpg", title: "Avengers: Infinity War 2", overview: "this is the overview 2"}
+    // ]
 
-    var movieRows = []
-    movies.forEach((movie) => {
-      console.log(movie.title)
-      const movieRow = <MovieRow movie = {movie} />
-      movieRows.push(movieRow)
-    })
+    // var movieRows = []
+    // movies.forEach((movie) => {
+    //   console.log(movie.title)
+    //   const movieRow = <MovieRow movie = {movie} />
+    //   movieRows.push(movieRow)
+    // })
 
-      this.state = {rows: movieRows}
-    
+    //   this.state = {rows: movieRows}
+
+    this.performSearch("avengers")
   }
+
+  performSearch(searchTerm) {
+    console.log("Perform search using moviedb")
+    const urlString = "https://api.themoviedb.org/3/search/movie?query=" + searchTerm + "&api_key=1b5adf76a72a13bad99b8fc0c68cb085"
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log("fetched data successfully")
+
+        const results = searchResults.results
+        // console.log(results[0])
+
+        var movieRows = []
+
+        results.forEach((movie) => {
+          movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
+          // console.log(movie.poster_path)
+          const movieRow = <MovieRow key={movie.id} movie={movie} />
+          movieRows.push(movieRow)
+        })
+
+        this.setState({rows: movieRows})
+      },
+      error: (xhr, status, err) => {
+        console.log("failed to fetch data")
+      }
+    })
+  }
+
+  searchChangeHandler(event) {
+    console.log(event.target.value)
+    const boundObject = this
+    const searchTerm = event.target.value
+    boundObject.performSearch(searchTerm)
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -35,7 +75,7 @@ class App extends Component {
             </tr>
           </tbody>
         </table>
-        <input type="text" placeholder="Enter Search term"/>
+        <input type="text" onChange={this.searchChangeHandler.bind(this)} placeholder="Enter Search term"/>
 
         {this.state.rows}
         
--- src/movieRow.js
+++ src/movieRow.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
 class MovieRow extends React.Component {
+    viewMovie() {
+        const url = "https://www.themoviedb.org/movie/" + this.props.movie.id
+        window.location.href = url
+    }
     render() {
         return  <table key={this.props.movie.id}>
         <tbody>
           <tr>
             <td><img src={this.props.movie.poster_src} alt="poster"/></td>
-            <td>{this.props.movie.title}</td>
-            <p>{this.props.movie.overview}</p>
+            <td>
+                <h3>{this.props.movie.title}</h3>
+                <p>{this.props.movie.overview}</p>
+                <input type="button" value="View" onClick={this.viewMovie.bind(this)}/>
+            </td>
           </tr>
         </tbody>
       </table>
